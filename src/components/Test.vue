63a9f3e68b6d9890754e1fd7ea328715bbfbf5e7
seolhee-choi <csheeee38@gmail.com>
2024-11-15 17:21:55 +0900

    1115 테스트 설문조사 페이지 생성

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,13 +1,10 @@
-<script lang="ts" setup>
+<script setup lang="ts">
 import { Model } from "survey-core";
 import { json } from "../../data/test_data.js";
 
-const survey = new Model({});
-survey.JSON = json;
+const testSurvey = new Model(json);
 </script>
 
 <template>
-  <h1>나와라</h1>
-<!--  <p> {{ survey }}</p>-->
-  <TestComponent :model="survey"></TestComponent>
+   <SurveyComponent :model="testSurvey"></SurveyComponent>
 </template>
